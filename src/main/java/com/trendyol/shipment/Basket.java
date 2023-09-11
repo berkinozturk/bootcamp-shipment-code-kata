d150ec4fa8821c4946f7d4301dbd79ccb2a52957
Berkin Öztürk <berkin.ozturk@std.izmirekonomi.edu.tr>
2023-09-11 16:37:21 +0300

    all tests passed

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,67 @@
 package com.trendyol.shipment;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        if (isEmptyBasket()) {
+            return null;
+        }
+
+        Map<ShipmentSize, Long> shipmentSizesCount = countShipmentSizes();
+
+        ShipmentSize largestSize = getLargestShipmentSize(shipmentSizesCount);
+
+        return determineShipmentSize(shipmentSizesCount, largestSize);
+    }
+
+    private boolean isEmptyBasket() {
+        return products == null || products.isEmpty();
+    }
+
+    private Map<ShipmentSize, Long> countShipmentSizes() {
+        return products.stream()
+                .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+    }
+
+    private ShipmentSize getLargestShipmentSize(Map<ShipmentSize, Long> shipmentSizesCount) {
+        return shipmentSizesCount.keySet().stream()
+                .max(ShipmentSize::compareTo)
+                .orElse(null);
+    }
+
+    private ShipmentSize determineShipmentSize(Map<ShipmentSize, Long> shipmentSizesCount, ShipmentSize largestSize) {
+        for (Map.Entry<ShipmentSize, Long> entry : shipmentSizesCount.entrySet()) {
+            if (entry.getValue() >= 3) {
+                return entry.getKey() == ShipmentSize.X_LARGE
+                        ? ShipmentSize.X_LARGE
+                        : getNextShipmentSize(entry.getKey());
+            }
+        }
+
+        if (products.size() < 3) {
+            return largestSize;
+        }
+
+        return largestSize;
+    }
+
+    private ShipmentSize getNextShipmentSize(ShipmentSize size) {
+        switch (size) {
+            case SMALL:
+                return ShipmentSize.MEDIUM;
+            case MEDIUM:
+                return ShipmentSize.LARGE;
+            case LARGE:
+                return ShipmentSize.X_LARGE;
+            default:
+                return ShipmentSize.X_LARGE;
+        }
     }
 
     public List<Product> getProducts() {
