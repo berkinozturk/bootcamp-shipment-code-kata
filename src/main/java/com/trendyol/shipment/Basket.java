af8cc7e1085fe2b91ce814d425a8fe22c93f8533
Berkin Öztürk <berkin.ozturk@std.izmirekonomi.edu.tr>
2023-09-11 17:36:05 +0300

    refactored due to have clean code

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,67 +1,65 @@
 package com.trendyol.shipment;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Basket {
-
-    private List<Product> products;
+    private List<Product> products = new ArrayList<>();
+    private static final Integer MINIMUM_SAME_SIZE_PRODUCT_COUNT = 3;
+    private static final Integer MINIMUM_PRODUCT_COUNT_FOR_LARGEST_SHIPMENT = 3;
 
     public ShipmentSize getShipmentSize() {
         if (isEmptyBasket()) {
             return null;
         }
 
-        Map<ShipmentSize, Long> shipmentSizesCount = countShipmentSizes();
+        Map<ShipmentSize, Long> productCountByShipmentSizeMap = getProductCountByShipmentSize();
 
-        ShipmentSize largestSize = getLargestShipmentSize(shipmentSizesCount);
-
-        return determineShipmentSize(shipmentSizesCount, largestSize);
+        return determineShipmentSize(productCountByShipmentSizeMap);
     }
 
     private boolean isEmptyBasket() {
-        return products == null || products.isEmpty();
+        return products.isEmpty();
     }
 
-    private Map<ShipmentSize, Long> countShipmentSizes() {
+    private Map<ShipmentSize, Long> getProductCountByShipmentSize() {
         return products.stream()
                 .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
     }
 
     private ShipmentSize getLargestShipmentSize(Map<ShipmentSize, Long> shipmentSizesCount) {
         return shipmentSizesCount.keySet().stream()
-                .max(ShipmentSize::compareTo)
+                .max(ShipmentSize.shipmentSizeComparator)
                 .orElse(null);
     }
 
-    private ShipmentSize determineShipmentSize(Map<ShipmentSize, Long> shipmentSizesCount, ShipmentSize largestSize) {
-        for (Map.Entry<ShipmentSize, Long> entry : shipmentSizesCount.entrySet()) {
-            if (entry.getValue() >= 3) {
+    private ShipmentSize determineShipmentSize(
+            Map<ShipmentSize, Long> productCountByShipmentSizeMap
+    ) {
+        ShipmentSize largestSize = getLargestShipmentSize(productCountByShipmentSizeMap);
+
+        if (products.size() < MINIMUM_PRODUCT_COUNT_FOR_LARGEST_SHIPMENT) {
+            return largestSize;
+        }
+
+        for (Map.Entry<ShipmentSize, Long> entry : productCountByShipmentSizeMap.entrySet()) {
+            if (entry.getValue() >= MINIMUM_SAME_SIZE_PRODUCT_COUNT) {
                 return entry.getKey() == ShipmentSize.X_LARGE
                         ? ShipmentSize.X_LARGE
                         : getNextShipmentSize(entry.getKey());
             }
         }
-
-        if (products.size() < 3) {
-            return largestSize;
-        }
-
         return largestSize;
     }
 
     private ShipmentSize getNextShipmentSize(ShipmentSize size) {
-        switch (size) {
-            case SMALL:
-                return ShipmentSize.MEDIUM;
-            case MEDIUM:
-                return ShipmentSize.LARGE;
-            case LARGE:
-                return ShipmentSize.X_LARGE;
-            default:
-                return ShipmentSize.X_LARGE;
-        }
+        return switch (size) {
+            case SMALL -> ShipmentSize.MEDIUM;
+            case MEDIUM -> ShipmentSize.LARGE;
+            default -> ShipmentSize.X_LARGE;
+        };
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -1,9 +1,23 @@
 package com.trendyol.shipment;
 
+import java.util.Comparator;
 public enum ShipmentSize {
 
-    SMALL,
-    MEDIUM,
-    LARGE,
-    X_LARGE
+    SMALL(0),
+    MEDIUM(1),
+    LARGE(2),
+    X_LARGE(3);
+
+    public final Integer size;
+
+    ShipmentSize(int size) {
+        this.size = size;
+    }
+
+    public static final Comparator<ShipmentSize> shipmentSizeComparator = new Comparator<ShipmentSize>() {
+        @Override
+        public int compare(ShipmentSize o1, ShipmentSize o2) {
+            return o1.size - o2.size;
+        }
+    };
 }
